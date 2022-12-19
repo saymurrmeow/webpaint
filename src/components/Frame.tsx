b10f3a036aaf7cc5215b0331b88fab640278233b
saymurrmeow <r.savinovskii@gmail.com>
2022-12-19 19:24:22 +0300

    feat: naive paint line

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,33 +1,41 @@
-import React, { useEffect, useRef } from 'react';
+import React, { MouseEvent, useEffect, useRef } from 'react';
 
 import './Frame.scss';
+
+const DEFAULT_CANVAS_WIDTH = 800;
+const DEFAULT_CANVAS_HEIGHT = 600;
 
 export const Frame = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const onDrawStart = () => {
-    /* TODO */
+  const handleStartDrawing = (e: MouseEvent<HTMLCanvasElement>) => {
+    const ctx = canvasRef.current?.getContext('2d');
+    ctx?.beginPath();
+    ctx!.strokeStyle = 'blue';
+    ctx?.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
   };
 
-  const onDrawEnd = () => {
-    /* TODO */
+  const handleEndDrawing = (e: MouseEvent<HTMLCanvasElement>) => {
+    const ctx = canvasRef.current?.getContext('2d');
+    ctx?.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    ctx?.stroke();
   };
 
-  const draw = () => {
-    onDrawStart();
-    onDrawEnd();
-    /* TODO */
-  };
+  const draw = () => { };
 
   useEffect(() => {
-    const reqId = requestAnimationFrame(draw);
-
-    return () => cancelAnimationFrame(reqId);
+    requestAnimationFrame(draw);
   }, []);
 
   return (
     <div className="frame__container">
-      <canvas ref={canvasRef} />
+      <canvas
+        width={DEFAULT_CANVAS_WIDTH}
+        height={DEFAULT_CANVAS_HEIGHT}
+        onMouseDown={handleStartDrawing}
+        onMouseUp={handleEndDrawing}
+        ref={canvasRef}
+      />
     </div>
   );
 };
