721bd8419703bc58bb105e8b203575321e9b214c
saymurrmeow <r.savinovskii@gmail.com>
2022-12-21 11:02:19 +0300

    wip: pushing active tool to drawing state

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -5,113 +5,87 @@ import './Frame.scss';
 const DEFAULT_CANVAS_WIDTH = 800;
 const DEFAULT_CANVAS_HEIGHT = 600;
 
-const createMouse = (canvas) => {
-  const mouse = {
-    x: 0,
-    y: 0,
-    left: false,
-    pLeft: false,
-    over: false,
-    startX: 0,
-    startY: 0,
-  };
-
-  mouse.tick = mouseTick;
-
-  function mouseTick() {
-    mouse.pLeft = mouse.left;
-  }
-
-  canvas.addEventListener('mouseenter', mouseenterHandler);
-  canvas.addEventListener('mousemove', mousemoveHandler);
-  canvas.addEventListener('mouseleave', mouseleaveHandler);
-  canvas.addEventListener('mousedown', mousedownHandler);
-  canvas.addEventListener('mouseup', mouseupHandler);
-
-  function mouseenterHandler(event) {
-    mouse.over = true;
-  }
-
-  function mousemoveHandler(event) {
-    const rect = canvas.getBoundingClientRect();
-    mouse.x = event.clientX - rect.left;
-    mouse.y = event.clientY - rect.top;
-  }
-
-  function mouseleaveHandler(event) {
-    mouse.over = false;
-  }
-
-  function mousedownHandler(event) {
-    if (event.buttons === 1) {
-      mouse.left = true;
-      mouse.startX = mouse.x;
-      mouse.startY = mouse.y;
-    }
-  }
-
-  function mouseupHandler(event) {
-    if (event.button === 0) {
-      mouse.left = false;
-    }
-  }
-
-  return mouse;
-};
-
 class Line {
-  private dx = 0;
-  private dy = 0;
+  x = 0;
+  y = 0;
+  dx = 0;
+  dy = 0;
 
-  constructor(private ctx: CanvasRenderingContext2D, private x: number, private y: number) { }
+  onDrawStart(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
   update(dx: number, dy: number) {
     this.dx = dx;
     this.dy = dy;
   }
 
-  draw() {
-    this.ctx.beginPath();
-    this.ctx.moveTo(this.x, this.y);
-    this.ctx.lineTo(this.dx, this.dy);
-    this.ctx.stroke();
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.dx, this.dy);
+    ctx.stroke();
+  }
+}
+
+const items = [];
+
+class Cursor {
+  private x = 0;
+  private y = 0;
+  private left = false;
+
+  private activeTool!: Line;
+
+  constructor(private canvasInstance: HTMLCanvasElement) {
+    canvasInstance.addEventListener('mousemove', this.handleMouseMove.bind(this));
+    canvasInstance.addEventListener('mousedown', this.handleMouseDown.bind(this));
+    canvasInstance.addEventListener('mouseup', this.handleMouseUp.bind(this));
+  }
+
+  handleMouseMove(e: MouseEvent) {
+    const { clientX, clientY } = e;
+    this.x = clientX;
+    this.y = clientY;
+    if (this.left) {
+      this.activeTool.update(this.x, this.y);
+      this.activeTool.draw(this.canvasInstance.getContext('2d'));
+    }
+  }
+
+  handleMouseDown() {
+    this.left = true;
+    this.activeTool = new Line();
+    this.activeTool.onDrawStart(this.x, this.y);
+  }
+
+  handleMouseUp() {
+    this.left = false;
+    items.push(this.activeTool);
   }
 }
 
 export const Frame = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const mouseRef = useRef();
 
-  const draw = () => {
+  const render = () => {
     requestAnimationFrame(tick);
-
-    const state = {
-      items: [],
-    };
 
     function tick() {
       requestAnimationFrame(tick);
-      const ctx = canvasRef.current!.getContext('2d')!;
-      const mouse = mouseRef.current;
-      ctx.save();
-      ctx?.clearRect(0, 0, DEFAULT_CANVAS_WIDTH, DEFAULT_CANVAS_HEIGHT);
-      const line = new Line(ctx, mouse.startX, mouse.startY);
-      if (mouse.left) {
-        mouse.tick();
-        ctx?.restore();
-        line.update(mouse.x, mouse.y);
-        line.draw();
-        state.items.push(line);
-      }
 
-      for (const item of state.items) {
-        item.draw();
+      const ctx = canvasRef.current!.getContext('2d')!;
+      ctx?.clearRect(0, 0, DEFAULT_CANVAS_WIDTH, DEFAULT_CANVAS_HEIGHT);
+
+      for (const item of items) {
+        item.draw(ctx);
       }
     }
   };
   useEffect(() => {
-    mouseRef.current = createMouse(canvasRef.current);
-    draw();
+    render();
+    const cursor = new Cursor(canvasRef.current);
   }, []);
 
   return (
