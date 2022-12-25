ad1feda30ac3dcdc30dbe81faa4a034eafac0fe1
saymurrmeow <r.savinovskii@gmail.com>
2022-12-25 21:19:58 +0300

    feat: get distance between points

--- src/model/Point.ts
+++ src/model/Point.ts
@@ -13,6 +13,10 @@ class Point {
   subtact(p: Point) {
     return new Point(this.x - p.x, this.y - p.y);
   }
+
+  distance(p: Point) {
+    return Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
+  }
 }
 
 export default Point;
